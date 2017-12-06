ebc876d16d5a8fa16a10660e396a2b77bb0c8f8e
bobrandt <31392557+bobrandt@users.noreply.github.com>
2017-12-06 15:12:26 -0800

    Final program for project

--- beerRetrieval.py
+++ beerRetrieval.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np
 from searchBeer import *
-from InvertedIndexFoodsearch import *
+#from InvertedIndexFoodsearch import *
 from locationSearch import *
 
-runBeerSearch()
-RunPairingSearch()
-searchBreweriesNearby()
+while True:
+	user_input = int(input("How would you like to search for a particular beer?\n[1] By name\n[2] By location\n[3] By food pairing\nEnter numeric value:"))
+
+	if user_input == 1:
+		runBeerSearch()
+		break
+	if user_input == 2:
+		searchBreweriesNearby()
+		break
+	#if user_input == 3:
+		#Ayo to add code
+		#break
+	else:
+		selection = input ("Error. Would you like to retry your search? [y/n]").lower()
+		if selection == 'n':
+			break
+		else:
+			continue
