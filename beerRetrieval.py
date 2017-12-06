5740e9d3ec69c31999a644c62f3744e4664d114f
sejalpopat <31431578+sejalpopat@users.noreply.github.com>
2017-12-06 14:31:23 -0800

    Update beerRetrieval.py

--- beerRetrieval.py
+++ beerRetrieval.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from searchBeer import *
 from InvertedIndexFoodsearch import *
+from locationSearch import *
 
 runBeerSearch()
 RunPairingSearch()
+searchBreweriesNearby()
