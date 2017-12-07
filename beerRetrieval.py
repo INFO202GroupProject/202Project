6dbb00f83a7ad2a83544bcf566c2a829a170ebfa
sejalpopat <31431578+sejalpopat@users.noreply.github.com>
2017-12-06 20:31:23 -0800

    wrap while loop in function to allow recursion
     wrap while loop in function to allow recursion (so we can return to the main set of prompts after following some line of querying)

--- beerRetrieval.py
+++ beerRetrieval.py
@@ -8,20 +8,27 @@ from InvertedIndexFoodsearch import *
 from locationSearch import *
 
 
-while True:
-	user_input = int(input("How would you like to search for a particular beer?\n[1] By name\n[2] By location\n[3] By food pairing\nEnter numeric value:"))
+def mainPrompt():
+	user_input = int(input("How would you like to search for a particular beer?\n[1] By name\n[2] By location\n[3] By food pairing\n[4] Quit \nEnter numeric value:"))
 
-	if user_input == 1:
-		runBeerSearch()
-	if user_input == 2:
-		searchBreweriesNearby()
-		break
-	if user_input == 3:
-		RunPairingSearch()
-	else:
-		selection = input ("Error. Would you like to search through our collection again? [y/n]").lower()
-		if selection == 'n':
+	while True:
+		if user_input == 1:
+			runBeerSearch()
+			mainPrompt()
+		if user_input == 2:
+			searchBreweriesNearby()
+			mainPrompt()
+		if user_input == 3:
+			RunPairingSearch()
+			mainPrompt()
+		if user_input == 4:
+			print("Exiting...")
 			break
 		else:
-			continue
-		
+			selection = input ("Error. Would you like to retry your search? [y/n]").lower()
+			if selection == 'n':
+				break
+			else:
+				continue
+
+mainPrompt()
